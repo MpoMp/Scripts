362d60ebabdbab0a00346b9376ecd5e46424c2cb
MpoMp <mpomp.aka.freestyler@gmail.com>
2014-09-04 14:50:22 +0300

    Init!

---
+++ Windowze/haskell_setup.bat
@@ -0,0 +1,6 @@
+@echo off
+start /d "C:\Program Files\Notepad++" notepad++.exe
+cd "C:\Repositories\"
+start "C:\WINDOWS\system32" cmd.exe "/c ""C:\Program Files\Git\bin\sh.exe" --login -i"
+cd "C:\Haskell\"
+cmd /k "ghci"
