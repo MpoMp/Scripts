c9604ccd76de40928a3cb62da6d28e96994c72a0
MpoMp <mpomp.aka.freestyler@gmail.com>
2014-09-11 11:44:50 +0300

    path fix

--- Windowze/haskell_setup.bat
+++ Windowze/haskell_setup.bat
@@ -2,5 +2,5 @@
 start /d "C:\Program Files\Notepad++" notepad++.exe
 cd "C:\Repositories\"
 start "C:\WINDOWS\system32" cmd.exe "/c ""C:\Program Files\Git\bin\sh.exe" --login -i"
-cd "C:\Repositories\learn_you_a_haskell_exercises\"
+cd "C:\Repositories\learn-you-a-haskell-exercises\"
 cmd /k "ghci"
