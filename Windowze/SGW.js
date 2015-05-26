cb47eb5e3aeb82749ca35185e58673c89f7df6bb
MpoMp <mpomp.aka.freestyler@gmail.com>
2015-05-26 12:27:53 +0300

    Fixed parsing

--- Windowze/SGW.js
+++ Windowze/SGW.js
@@ -1,9 +1,11 @@
 var elem = $x('/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div[2]/text()')
 var wt = elem[0].textContent
+wt = wt.replace(/,/g, '')
 var won = parseFloat(wt.replace( /^\D+/g, ''))
 
 elem = $x('/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[3]/div[2]/span/text()')
 wt = elem[0].textContent
+wt = wt.replace(/,/g, '')
 var sent = parseFloat(wt.replace( /^\D+/g, ''))
 
 var ratio = won/sent
