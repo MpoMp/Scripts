3a2e76c071fa42b9fb0d358c44ae820fc932c228
MpoMp <mpomp.aka.freestyler@gmail.com>
2015-05-27 12:16:43 +0300

    Update SGW.js

--- Windowze/SGW.js
+++ Windowze/SGW.js
@@ -1,4 +1,47 @@
-function getRatio(memberURL) {
+/////////////////////////
+//Sequential code (run on one page manually)
+/////////////////////////
+
+//starting from whitelist page
+this.document.location = "http://www.steamgifts.com/account/manage/whitelist/search?page=1"
+
+//get all whitelisted in current page
+var elem = $x('/html/body/div[1]/div/div/div[2]/div[3]/div[1]')
+var nums = elem[0].textContent.match( /\d+/g )
+n = nums [1] - nums[0] + 1
+  
+var members = []
+
+for	(i = 1; i <= n; i++) {
+  var xpath = $x('/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div['.concat(i, ']/div/div[2]/a'))
+  members.push(xpath[0].textContent)
+}
+
+//check ratios
+for	(i = 0; i < n; i++) {
+  this.document.location = "http://www.steamgifts.com/user/".concat(members[i])
+  var elem = $x('/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div[2]/text()')
+  var wt = elem[0].textContent
+  wt = wt.replace(/,/g, '')
+  var won = parseFloat(wt.replace( /^\D+/g, ''))
+
+  elem = $x('/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[3]/div[2]/span/text()')
+  wt = elem[0].textContent
+  wt = wt.replace(/,/g, '')
+  var sent = parseFloat(wt.replace( /^\D+/g, ''))
+
+  var ratio = won/sent
+  if (ratio > 0.33){
+    console.log('Bad ratio:'.concat(members[i]));
+  }
+  
+}
+
+////////////////////////////////////////////////
+//Code in modules, for possible extension...//
+///////////////////////////////////////////////
+
+/*function getRatio(memberURL) {
   var elem = $x('/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div[2]/text()')
   var wt = elem[0].textContent
   wt = wt.replace(/,/g, '')
@@ -22,7 +65,7 @@ function getMembers() {
   var members = []
 
   for	(i = 1; i <= n; i++) {
-    var xpath = '/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div['.concat(i, ']/div/div[2]/a');
+    var xpath = $x('/html/body/div[1]/div/div/div[2]/div[2]/div[2]/div['.concat(i, ']/div/div[2]/a'))
     members.push(xpath[0].textContent)
   }
   
@@ -43,5 +86,4 @@ for	(i = 1; i <= pages; i++) {
   this.document.location = "http://www.steamgifts.com/account/manage/whitelist/search?page=".concat(i)
 }
 
-////for each member get ratio
-////if bad ratio, add to reported array
+*/
